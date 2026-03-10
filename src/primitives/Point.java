92a1451cdbcadc216325c71a2d6b641bf33a277e
Moshe Diamandi <moshemor@g.jct.ac.il>
2026-03-10 11:21:47 +0200

    Add base classes for geometries and enhancements to primitives
    Signed-off-by: Moshe Diamandi <moshemor@g.jct.ac.il>

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -2,38 +2,79 @@ package primitives;
 
 import java.util.Objects;
 
+/**
+ * Represents a 3D point in space.
+ */
 public class Point {
+    /**
+     * The coordinates of the point.
+     */
     protected final Double3 _xyz;
-    public static Double3 ZERO = new Double3(0, 0, 0);
-//    public static Double3 ONE = new Double3(1, 1, 1);
 
+    /**
+     * The origin point (0,0,0).
+     */
+    public static final Point ZERO = new Point(Double3.ZERO);
+
+    /**
+     * The unit point (1,1,1).
+     */
+//    public static final Point ONE = new Point(Double3.ONE);
+
+    /**
+     * Constructor for Point using 3 coordinates.
+     * * @param x the x coordinate
+     * @param y the y coordinate
+     * @param z the z coordinate
+     */
     public Point(double x, double y, double z) {
         this(new Double3(x, y, z));
     }
 
-    public Point(Double3 _xyz) {
-        this._xyz = _xyz;
+    /**
+     * Constructor for Point using a Double3 object.
+     * * @param xyz the Double3 coordinates
+     */
+    public Point(Double3 xyz) {
+        this._xyz = xyz;
     }
 
-
-
+    /**
+     * Subtracts a point from this point, returning a vector from the other point to this point.
+     * * @param p the point to subtract
+     * @return a new Vector representing the difference
+     */
     public Vector subtract(Point p) {
         return new Vector(_xyz.subtract(p._xyz));
     }
 
+    /**
+     * Adds a vector to this point, returning a new point.
+     * * @param v the vector to add
+     * @return a new Point representing the sum
+     */
     public Point add(Vector v) {
         return new Point(_xyz.add(v._xyz));
     }
 
+    /**
+     * Computes the squared distance between this point and another point.
+     * * @param p the other point
+     * @return the squared distance
+     */
     public double distanceSquared(Point p) {
         double deltaX = _xyz._d1() - p._xyz._d1();
         double deltaY = _xyz._d2() - p._xyz._d2();
         double deltaZ = _xyz._d3() - p._xyz._d3();
 
-
-        return deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ;
+        return (deltaX * deltaX) + (deltaY * deltaY) + (deltaZ * deltaZ);
     }
 
+    /**
+     * Computes the distance between this point and another point.
+     * * @param p the other point
+     * @return the distance
+     */
     public double distance(Point p) {
         return Math.sqrt(distanceSquared(p));
     }
@@ -41,7 +82,8 @@ public class Point {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Point point)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
         return _xyz.equals(point._xyz);
     }
 
--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,14 +1,57 @@
 package primitives;
 
+import java.util.Objects;
+
+/**
+ * Represents a ray in 3D space, which is a half-line defined by a starting point and a direction vector.
+ */
 public class Ray {
+    /**
+     * The starting point of the ray.
+     */
     private final Point _head;
+
+    /**
+     * The normalized direction vector of the ray.
+     */
     private final Vector _direction;
 
+    /**
+     * Constructs a ray with a given starting point and direction.
+     * The direction vector is normalized before assignment.
+     *
+     * @param head      the starting point of the ray
+     * @param direction the direction vector of the ray
+     */
     public Ray(Point head, Vector direction) {
         _head = head;
         _direction = direction.normalize();
     }
-    public Vector direction(){
+
+    /**
+     * Returns the normalized direction vector of the ray.
+     *
+     * @return the direction vector
+     */
+    public Vector direction() {
         return _direction;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Ray other = (Ray) obj;
+        return _head.equals(other._head) && _direction.equals(other._direction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_head, _direction);
+    }
+
+    @Override
+    public String toString() {
+        return "Ray{" + "head=" + _head + ", direction=" + _direction + '}';
     }
 }
