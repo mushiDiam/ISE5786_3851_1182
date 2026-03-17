86f6bbd3fe35a25163906d599c170b97f35c79d9
Moshe Diamandi <moshemor@g.jct.ac.il>
2026-03-17 15:48:28 +0200

    Refactor PointTests: improve formatting, enhance import statements, and add JUnit 5 dependencies
    Signed-off-by: Moshe Diamandi <moshemor@g.jct.ac.il>

---
+++ unitTests/primitives/PointTests.java
@@ -0,0 +1,34 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PointTests {
+
+    @Test
+    void testSubtract1() {
+        Point p1 = new Point(1, 2, 3);
+        Point p2 = new Point(-1, 2, 4);
+        assertEquals(new Vector(-2, 0, 1), p2.subtract(p1), "Not equal");
+    }
+
+    @Test
+    void testSubtract2() {
+        Point p1 = new Point(1, 2, 3);
+        assertThrows(IllegalArgumentException.class, () -> p1.subtract(p1), "Should be invalid");
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void testDistanceSquared() {
+    }
+
+    @Test
+    void testDistance() {
+    }
+}
